db11247ea9a441cbe140ad9452fa0a373dda5f89
Sherry <sherrylin42@gmail.com>
2023-03-24 16:08:36 -0400

    chore: change download order

--- install_azure_acquire.sh
+++ install_azure_acquire.sh
@@ -2,13 +2,13 @@
 
 set -e
 
+# Install curl
+sudo apt install curl
+
 # Install miniconda
 curl -L https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -o "$HOME/miniconda3_latest.sh"
 chmod +x $HOME/miniconda3_latest.sh
 $HOME/miniconda3_latest.sh
-
-# Install curl
-sudo apt install curl
 
 # Install build-essential and ffmpeg
 sudo apt install build-essential
