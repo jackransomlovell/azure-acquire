2103c5438129ae8c4b74ecba8d00030333a595f6
Sherry <sherrylin42@gmail.com>
2023-11-29 11:14:48 -0500

    chore: restructure installation script

--- install_azure_acquire.sh
+++ install_azure_acquire.sh
@@ -5,11 +5,6 @@ set -e
 # Install curl
 sudo apt install curl
 
-# Install miniconda
-curl -L https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -o "$HOME/miniconda3_latest.sh"
-chmod +x $HOME/miniconda3_latest.sh
-$HOME/miniconda3_latest.sh
-
 # Install build-essential and ffmpeg
 sudo apt install build-essential
 sudo apt install ffmpeg
@@ -26,3 +21,8 @@ sudo apt install k4a-tools=1.3.0
 # Finish the device setup
 wget https://raw.githubusercontent.com/microsoft/Azure-Kinect-Sensor-SDK/develop/scripts/99-k4a.rules
 sudo mv 99-k4a.rules /etc/udev/rules.d/
+
+# Install miniconda
+curl -L https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -o "$HOME/miniconda3_latest.sh"
+chmod +x $HOME/miniconda3_latest.sh
+$HOME/miniconda3_latest.sh
